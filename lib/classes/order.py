83ad1b0ae64f874405cb0e4437314a67acf14378
footballjones <footballjones308@gmail.com>
2023-05-30 09:30:26 -0400

    Completed

--- lib/classes/order.py
+++ lib/classes/order.py
@@ -1,7 +1,42 @@
 
 class Order:
+    all = []
 
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        Order.all.append(self)
+
+        customer.orders(self)
+        customer.coffees(coffee)
+
+        coffee.orders(self)
+        coffee.customers(customer)
+
+        @property
+        def customer(self):
+            return self._customer
+        
+        @customer.setter
+        def customer(self, customer):
+            from classes.customer import Customer
+            if isinstance(customer, Customer):
+                self._customer = customer
+            return self._customer
+        
+        @property
+        def order(self):
+            return self._order
+        
+        @order.setter
+        def order(self, order):
+            from classes.order import Order
+            if isinstance(order, Order):
+                self._order = order
+            else:
+                raise Exception
+
+
+
+
